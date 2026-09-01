aa6788a83817d65c42d449d8223ce069e4d0f802
MomoPewpew <marijntepas@gmail.com>
2026-09-01 19:30:13 +0200

    Update build configurations and dependencies for pathfinder modules

--- pathfinder-bukkit/build.gradle.kts
+++ pathfinder-bukkit/build.gradle.kts
@@ -45,7 +45,7 @@ dependencies {
     testImplementation("com.mojang:brigadier:1.0.18")
 
     // Commands
-    api("dev.jorel:commandapi-bukkit-shade:9.5.3")
+    api("dev.jorel:commandapi-bukkit-shade:9.7.0")
 
     // Statistics
     implementation("org.bstats:bstats-bukkit:3.0.1")
--- pathfinder-editmode/build.gradle.kts
+++ pathfinder-editmode/build.gradle.kts
@@ -46,7 +46,7 @@ dependencies {
     implementation("xyz.xenondevs:particle:1.8.4")
 
     // Client ArmorStands
-    implementation("de.cubbossa:ClientEntities:1.3.6")
+    implementation(files("../libs/ClientEntities-1.3.6.jar"))
 }
 
 tasks {
